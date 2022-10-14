c57499ab2880fae27dba1144fb7f4038c63478ec
YuliaDuk <yule444ka1234@mail.ru>
2022-10-14 13:14:10 +0300

    user interface for telephone book

---
+++ Sem7_HW/UI.py
@@ -0,0 +1,10 @@
+def get_name():
+    return input('Введите ФИО: ')
+
+def get_number():
+    return input("Введите номер телефона: ")
+
+def show_contact(a,b):
+    print(f'Добавлен следующий контакт: \n {a} || {b}')
+
+
